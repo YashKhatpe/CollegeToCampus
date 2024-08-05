e7985d921a43b1c4464ad15756385c2ec2cdb0c3
krishna-254 <91021227+krishna-254@users.noreply.github.com>
2024-08-05 23:04:03 +0530

    user model change and req file

--- backend/college_to_campus/user/models.py
+++ backend/college_to_campus/user/models.py
@@ -9,14 +9,30 @@ def validate_mobile_number(value):
 
 class user(models.Model):
     GENDER = [
-        ('male', 'male'),
-        ('female','female')
+        ('Male', 'Male'),
+        ('Female','Female'),
+        ('Others','Others')
     ]
-    name = models.CharField(max_length=100)
+    USER_TYPE = [
+        ('Student', 'Student'),
+        ('Employee', 'Employee')
+    ]
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    profilePicture = models.ImageField(blank=True, null=True)
+    
     email = models.EmailField(max_length=255)
-    phone = models.CharField(max_length=10, validators=[validate_mobile_number])
+    contactNumber = models.CharField(max_length=10, validators=[validate_mobile_number])
     gender = models.CharField(max_length=10,choices= GENDER)
-    address = models.CharField(max_length=255)
-    college = models.CharField(max_length=255)
-    degree = models.CharField(max_length=255)
-    cgpa = models.FloatField()
+    city = models.CharField(max_length=255)
+    
+    clgName = models.CharField(max_length=255,blank=True, null=True)
+    degree = models.CharField(max_length=255,blank=True, null=True)
+    
+    cmpName = models.CharField(max_length=255,blank=True, null=True)
+    cmpUrl = models.CharField(max_length=255,blank=True, null=True)
+    desg =  models.CharField(max_length=255,blank=True, null=True)
+    
+    user_type = models.CharField(choices=USER_TYPE,max_length=20)
+
+
